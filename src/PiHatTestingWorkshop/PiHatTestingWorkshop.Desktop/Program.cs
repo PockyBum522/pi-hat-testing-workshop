22122dadbe35366587267f262b5e8a8442640d23
DSikes <pockybum522@gmail.com>
2025-09-20 09:15:26 -0400

    Strike that, reverse it

--- src/PiHatTestingWorkshop/PiHatTestingWorkshop.Desktop/Program.cs
+++ src/PiHatTestingWorkshop/PiHatTestingWorkshop.Desktop/Program.cs
@@ -36,7 +36,7 @@ public static class Program
                 runAsGuiArgumentFound = true;
         }
 
-        if (runAsGuiArgumentFound)
+        if (!runAsGuiArgumentFound)
         {
             await runApplicationAsCliOnly();
         }
